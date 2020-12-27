49b2bbc7520d5d9fba6e333a81080f1ae4a5bb52
Ankita <ankita.dharne@juspay.in>
2020-12-27 23:31:43 +0530

    wired session, added log status check and logout functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
 import express, {Response,Request } from 'express';
 import {router} from './routes/LoginRoutes'
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({keys: ['lasdfij']}));
 app.use(router);
 
 app.listen(3000,() => {
     console.log('Listening on port 3000');
-}); 
+});
